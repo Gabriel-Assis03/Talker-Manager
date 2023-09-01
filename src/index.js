e6d0ba81bd02457b3ab0e975347f1dc745e1d80e
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-09-01 19:30:50 -0300

    requisito 1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 
 const app = express();
+const fs = require('fs/promises');
+
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
-// não remova esse endpoint, e para o avaliador funci onar
+// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const data = await fs.readFile('./src/talker.json', { encoding: 'utf-8' });
+  const talkers = JSON.parse(data);
+  if (talkers) {
+    res.status(200).json(talkers);
+  } else {
+    res.status(200).json([]);
+  }
 });
 
 app.listen(PORT, () => {
