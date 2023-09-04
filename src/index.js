9930ed661bd00401c715fa726aa5364c476eb69c
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-09-04 19:13:28 -0300

    requisito 4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs/promises');
 const crypto = require('crypto');
+const validator = require('validator'); // biblioteca para validações
 
 app.use(express.json());
 
@@ -43,7 +44,13 @@ app.get('/talker/:id', async (req, res) => {
 const validateLogin = (req, res, next) => {
   const { email, password } = req.body;
   if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
   if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
   next();
 };
 
